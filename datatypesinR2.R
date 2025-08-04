12513888cef5645879a5e27ebf0a75fc734335aa
Kiyoshi C. Rio <86674319+Kiyoshi8@users.noreply.github.com>
2025-08-04 17:53:07 +0800

    Update datatypesinR2.R

--- datatypesinR2.R
+++ datatypesinR2.R
@@ -24,7 +24,7 @@
              # complex, character, list, closure
 
 # lets create some vectors with the most common type of data
-a = (2:5) 
+a = (1:4) 
 b = rep(1.50,4)
 c = c("wed", "tue", "sat")
 d = c(1+7i, 2+4i)
@@ -63,5 +63,6 @@ f
 
 # in this case we coerced logical vector e
 # to character vector f
+
 
 
